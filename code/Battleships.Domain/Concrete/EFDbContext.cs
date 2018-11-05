b1f9a5a98526d871a121e702c9a342510a15bb78
dwood96x <darksoldierx@gmail.com>
2018-11-04 23:00:25 -0500

    Updated all dependencies. Added database EFDB Context.

---
+++ code/Battleships.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Battleships.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Battleships.Domain.Concrete
+{
+    public class EFDbContext : System.Data.Entity.DbContext
+    {
+        public DbSet<Stats> Stats { get; set; }
+    }
+}
---
+++ code/Battleships.Domain/Concrete/EFStatsRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Battleships.Domain.Abstract;
+using Battleships.Domain.Entities;
+
+namespace Battleships.Domain.Concrete
+{
+    public class EFStatsRepository : IStatsRepository
+    {
+        private EFDbContext context = new EFDbContext();
+
+        public IEnumerable<Stats> Stats
+        {
+            get { return context.Stats; }
+        }
+    }
+}
